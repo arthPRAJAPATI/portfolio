01d160484f205714aa31d889b5515a49228c26f9
arthPRAJAPATI <prajapatiarth@gmail.com>
2023-04-29 15:57:34 -0400

    enhanced css

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { EducationComponent } from './education/education.component';
 import { ContactComponent } from './contact/contact.component';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ContactComponent } from './contact/contact.component';
     ProjectsComponent,
     ExperienceComponent,
     EducationComponent,
-    ContactComponent
+    ContactComponent,
+    ScrollToTopComponent
   ],
   imports: [
     BrowserModule
---
+++ my-portfolio/src/app/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  templateUrl: './scroll-to-top.component.html',
+  styleUrls: ['./scroll-to-top.component.css']
+})
+export class ScrollToTopComponent {
+  isShow: boolean = false;
+  topPosToStartShowing = 500;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
+}
+
